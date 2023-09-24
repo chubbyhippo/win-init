02e91744d232c2e253f6ddb4055c254e3cbec528
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2023-09-24 12:30:58 +0700

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -2,6 +2,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # install programs
 choco install git -y
+choco install nuget.commandline -y
 # refresh
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
