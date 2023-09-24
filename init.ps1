f2fe31546138bf17583cd0ab51bdc20a50ac8943
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2023-09-24 19:02:02 +0700

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -3,8 +3,16 @@ Install-PackageProvider -Name NuGet -Force
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # install programs
 choco install git -y
+choco install autohotkey -y
 # refresh
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+
+git clone https://github.com/chubbyhippo/ahk.git $Home\ahk
+git clone 
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\CapsToChangeInputLanguage.lnk")
+$Shortcut.TargetPath = "$Home\ahk\CapsToChangeInputLanguage.ahk"
+$Shortcut.Save()
 
 git clone https://github.com/LeDragoX/Win-Debloat-Tools.git
 Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force; ls -Recurse *.ps*1 | Unblock-File; .\Win-Debloat-Tools\"WinDebloatTools.ps1" 'CLI'
