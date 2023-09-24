a76c0f78bc058aa768aa4c48d50d38838e5c53e1
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2023-09-24 13:04:10 +0700

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,1 +1,1 @@
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%~dp0init.ps1"' -Verb RunAs}"
+PowerShell -NonInteractive -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%~dp0init.ps1"' -Verb RunAs}"
