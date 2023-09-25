3dea87b70dfb4fd2d6ed875d91a535fefa5b69ae
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2023-09-25 15:12:59 +0700

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,4 +1,4 @@
-Set-WinUserLanguageList en-US,th-TH
+Set-WinUserLanguageList en-US,th-TH -Force
 Install-PackageProvider -Name NuGet -Force
 # install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
