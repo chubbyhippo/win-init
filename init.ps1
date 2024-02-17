53a9d4b38d2153837ac0d18dc4641e8ec885a49a
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-02-17 09:47:42 +0700

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -14,5 +14,5 @@ $Shortcut = $WshShell.CreateShortcut("$Home\AppData\Roaming\Microsoft\Windows\St
 $Shortcut.TargetPath = "$Home\ahk\CapsToChangeInputLanguage.ahk"
 $Shortcut.Save()
 
-git clone https://github.com/LeDragoX/Win-Debloat-Tools.git
-Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force; ls -Recurse *.ps*1 | Unblock-File; .\Win-Debloat-Tools\"WinDebloatTools.ps1" 'CLI'
+# git clone https://github.com/LeDragoX/Win-Debloat-Tools.git
+# Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force; ls -Recurse *.ps*1 | Unblock-File; .\Win-Debloat-Tools\"WinDebloatTools.ps1" 'CLI'
