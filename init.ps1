f27086b6bc55bc8ea6c08918dca7b11fa1caf4a7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-02-20 15:39:34 +0700

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -4,8 +4,7 @@ Set-WinUserLanguageList en-US,th-TH -Force
 # install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # install programs
-install git -y
-install autohotkey -y
+choco install git autohotkey -y
 # refresh
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
