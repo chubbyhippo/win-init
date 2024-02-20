766bdf1174d1e1a44d22704a5d59ddf0ceb8d37b
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-02-20 15:14:36 +0700

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -20,5 +20,5 @@ $Shortcut.TargetPath = "$Home\CapsToChangeInputLanguage.ahk"
 $Shortcut.Save()
 
 # debloat
-git clone https://github.com/Raphire/Win11Debloat.git
-Set-ExecutionPolicy Unrestricted -Scope Process -Force; .\Win11Debloat\"Win11Debloat.ps1" -Silent -RunDefaults -RemoveW11Outlook -ClearStart -ShowHiddenFolders
+git clone https://github.com/Raphire/Win11Debloat.git "$Home\Win11Debloat"
+Set-ExecutionPolicy Unrestricted -Scope Process -Force; ".$Home\Win11Debloat\Win11Debloat.ps1" -Silent -RunDefaults -RemoveW11Outlook -ClearStart -ShowHiddenFolders
