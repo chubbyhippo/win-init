045a8a25f9dc1fca23b323b71075e7d7e0e7eaad
ch <mkmsacc@outlook.com>
2024-02-21 12:10:27 +0700

    Correct commit message: Remove clean-up operations from init.ps1
    This commit eliminates the line of code in init.ps1 that deletes the downloaded file and the extracted folder after the Run.bat file has been executed. This simplifies the script and prevents potential issues caused by the deletion of these items.

--- init.ps1
+++ init.ps1
@@ -3,5 +3,3 @@ $Uri='https://github.com/chubbyhippo/windows/archive/refs/heads/main.zip'
 $WebClient.DownloadFile($Uri, "$Home\downloads\windows-main.zip")
 Expand-Archive -Path $Home\downloads\windows-main.zip
 cmd.exe -/c windows-main\windows-main\Run.bat
-rm -fo -r "$Home\downloads\windows-main.zip"
-rm -fo -r "$Home\windows-main"
