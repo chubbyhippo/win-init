0dfe9bb2d09e77b7e01ff113bbae246f80c04d2e
ch <mkmsacc@outlook.com>
2024-02-21 11:03:33 +0700

    Update PowerShell command in Run.bat
    The command in the Run.bat file was modified from init.ps1 to install.ps1. This change was made to direct the script to initiate with the 'install' argument switching from the previous 'init' setting, improving the clarity of the script's purpose.

--- Run.bat
+++ Run.bat
@@ -1,1 +1,1 @@
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%~dp0init.ps1"' -Verb RunAs}"
+PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%~dp0install.ps1"' -Verb RunAs}"
