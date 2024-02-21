0a6d0d758fda4dfb078f95800f0a30dda9f0eec5
ch <mkmsacc@outlook.com>
2024-02-21 15:01:57 +0700

    Script execution sequence optimized in init.ps1
    In this update, the batch file is now executed as the final step in the download and expand sequence, providing more efficient and consistent performance. Additionally, the removal of the downloaded zip file and its subsequent folder happens post-batch execution, further enhancing the cleanup process.

--- init.ps1
+++ init.ps1
@@ -1,7 +1,7 @@
 $WebClient=New-Object Net.WebClient
 $Uri='https://github.com/chubbyhippo/windows/archive/refs/heads/main.zip'
 $WebClient.DownloadFile($Uri, "$Home\downloads\windows-main.zip")
-Expand-Archive -Path $Home\downloads\windows-main.zip
-cmd.exe -/c windows-main\windows-main\Run.bat
+Expand-Archive -Path "$Home\downloads\windows-main.zip" -DestinationPath "$Home\windows-main"
+cmd.exe -/c $Home\windows-main\windows-main\Run.bat
 rm -fo -r "$Home\downloads\windows-main.zip"
 rm -fo -r "$Home\windows-main"
