dd5185974d88dec0429b883599c09ff838a97c94
ch <mkmsacc@outlook.com>
2024-02-21 14:29:43 +0700

    Updated execution sequence in install.ps1
    The updated script now runs the Win11Debloat script as a final step in the process, enhancing run consistency. The batch file will now execute last, allowing for more streamlined execution. Moreover, the removal of the zip file and folder after batch file execution has been resolved too.

--- install.ps1
+++ install.ps1
@@ -20,5 +20,4 @@ $Shortcut.TargetPath = "$Home\CapsToChangeInputLanguage.ahk"
 $Shortcut.Save()
 
 git clone https://github.com/Raphire/Win11Debloat.git
-#Set-ExecutionPolicy Unrestricted -Scope Process -Force; .\Win11Debloat\"Win11Debloat.ps1" -Silent -RunDefaults -RemoveW11Outlook -ClearStart -ShowHiddenFolders
-Start-Process -NoNewWindow -FilePath PowerShell -ArgumentList "-ExecutionPolicy Unrestricted -Scope Process -Force; .\Win11Debloat\"Win11Debloat.ps1" -Silent -RunDefaults -RemoveW11Outlook -ClearStart -ShowHiddenFolders" -Wait
+Set-ExecutionPolicy Unrestricted -Scope Process -Force; .\Win11Debloat\"Win11Debloat.ps1" -Silent -RunDefaults -RemoveW11Outlook -ClearStart -ShowHiddenFolders
