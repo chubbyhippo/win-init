9e0d30aa489b987aed5aee124b7547c3e30a7d6e
ch <mkmsacc@outlook.com>
2024-02-22 14:06:06 +0700

    Remove unused comments and add debloat in install.ps1
    This commit removes the commented-out installation commands for Git and Autohotkey in the Powershell script. Also, a new debloating operation has been added to the script, aiming to remove bloatware from Windows 11.

--- install.ps1
+++ install.ps1
@@ -1,7 +1,6 @@
 Set-WinUserLanguageList en-US,th-TH -Force
 Install-PackageProvider -Name NuGet -Force
-# install git
-#winget install --id Git.Git -e -h --accept-package-agreements --accept-source-agreements
+
 # install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # install programs
@@ -24,7 +23,6 @@ choco install vscode -y
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
 # install autohotkey
-#winget install -e -h Autohotkey.Autohotkey
 $WebClient=New-Object Net.WebClient
 $Uri='https://raw.githubusercontent.com/chubbyhippo/windows/main/CapsToChangeInputLanguage.ahk'
 $WebClient.DownloadFile($Uri, "$Home\CapsToChangeInputLanguage.ahk")
@@ -33,6 +31,7 @@ $Shortcut = $WshShell.CreateShortcut("$Home\AppData\Roaming\Microsoft\Windows\St
 $Shortcut.TargetPath = "$Home\CapsToChangeInputLanguage.ahk"
 $Shortcut.Save()
 
+# debloat
 git clone https://github.com/Raphire/Win11Debloat.git
 Set-ExecutionPolicy Unrestricted -Scope Process -Force; .\Win11Debloat\"Win11Debloat.ps1" -Silent -RunDefaults -RemoveW11Outlook -ClearStart -ShowHiddenFolders
 rm -fo -r Win11Debloat
