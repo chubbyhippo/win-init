402a9dbcc116b3a2234d8434f2b9aa69da517eca
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-02-29 09:11:10 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -13,7 +13,7 @@ choco install googlechrome -y
 choco install inkscape -y
 choco install insomnia-rest-api-client -y
 choco install jetbrainstoolbox -y
-choco install libericajdk -y
+choco install openjdk -y
 choco install liberica-mission-control -y
 choco install nanazip -y
 choco install neovim -y
