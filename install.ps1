e661673c0d37a2f4fabb31fce6b619aa17995a24
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-09-27 08:54:49 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,15 +1,17 @@
 Set-WinUserLanguageList en-US,th-TH -Force
 Install-PackageProvider -Name NuGet -Force
 
-# setup autohotkey
-$url = "https://raw.githubusercontent.com/chubbyhippo/windows/main/remap.ahk"
-$destinationPath = "$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\remap.ahk"
-Invoke-WebRequest -Uri $url -OutFile $destinationPath
+
 
 # debloat
 git clone https://github.com/Raphire/Win11Debloat.git
 Set-ExecutionPolicy Unrestricted -Scope Process -Force; .\Win11Debloat\"Win11Debloat.ps1" -Silent -RunDefaults -RemoveW11Outlook -ClearStart -ShowHiddenFolders
 rm -fo -r Win11Debloat
+
+# setup autohotkey
+$url = "https://raw.githubusercontent.com/chubbyhippo/windows/main/remap.ahk"
+$destinationPath = "$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\remap.ahk"
+Invoke-WebRequest -Uri $url -OutFile $destinationPath
 
 # ideavim
 Invoke-RestMethod https://raw.githubusercontent.com/chubbyhippo/.ideavimrc/main/install.ps1 | Invoke-Expression
